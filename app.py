8bba36fb6b166d66584cda73f74e16d557e1bd49
defyingdemonprogram <99237387+defyingdemonprogram@users.noreply.github.com>
2023-08-06 18:30:02 +0000

    Simple display of data using flask

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from pipeline import MyDatabase
+from jinja2 import Environment, PackageLoader
 
 app = Flask(__name__)
+
+# Create a Jinja2 environment
+env = Environment(loader=PackageLoader("app"))
+
+# Define the custom 'enumerate' filter
+def enumerate_filter(iterable, start=0):
+    return zip(range(start, len(iterable) + start), iterable)
+
+env.filters['enumerate'] = enumerate_filter
+
+
 
 @app.route('/')
 def index():
@@ -8,11 +21,19 @@ def index():
 
 @app.route('/book')
 def book_view():
-    return render_template('book.html')
+    database = MyDatabase(db_name="bookdb")
+    tables = database.get_tables_name()
+    columns = database.get_column_names(table_name=tables[0])
+    records = database.get_records(table=tables[0])
+    return render_template('book.html', columns=columns, records=records)
 
 @app.route('/quotes')
 def quotes_view():
-    return render_template('quotes.html')
+    database = MyDatabase(db_name="quotesdb")
+    tables = database.get_tables_name()
+    columns = database.get_column_names(table_name=tables[0])
+    records = database.get_records(table=tables[0])
+    return render_template('quotes.html', columns=columns, records=records)
 
 @app.route('/contacts')
 def contacts_view():
--- pipeline.py
+++ pipeline.py
@@ -1,18 +1,72 @@
 import os
+import configparser
 import mysql.connector
 
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# config variable
+db_host = config["DATABASE"]["host"]
+db_user = config["DATABASE"]["user"]
+db_password = config["DATABASE"]["password"]
+
 class MyDatabase:
-    def __init__(self):
-        self.conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="bookdb"
-        )
-        self.cur = self.conn.cursor()
-        a = self.cur.execute("SELECT * from books limit ")
-        print(a)
-        print(self.conn)
+    def __init__(self, db_name="bookdb"):
+        try:
+            os.system("sudo service mysql start")
+            self.conn = mysql.connector.connect(
+                host=db_host,
+                user=db_user,
+                password=db_password,
+                database=db_name
+            )
+            self.cur = self.conn.cursor()
+            self.db_name = db_name
+        except mysql.connector.Error as err:
+            raise (f"Cannot connect to database: {err}")
+        self.sys_table = ['information_schema', 'mysql', 'performance_schema', 'sys']
+
+    def get_tables_name(self):
+        self.cur.execute(f"SHOW TABLES;")
+        all_user_tables = [table[0].decode('utf-8') for table in self.cur.fetchall() \
+                           if table[0] not in self.sys_table]
+        if not len(all_user_tables):
+            if self.db_name=="bookdb":
+                os.system("cd bookscrape")
+                os.system("scrapy crawl bookspider")
+            if self.db_name=="quotesdb":
+                os.system("cd quotes_scrape")
+                os.system("scrapy crawl quotespider")
+        return all_user_tables
+
+    def get_column_names(self, table_name):
+        self.cur.execute(f"SHOW COLUMNS FROM {table_name}")
+        # return info of table columns
+        columns = [column[0] for column in self.cur.fetchall()]
+        return columns
+    
+    def get_records(self, table, limit=None):
+        try:
+            if limit is None:
+                self.cur.execute(f"""SELECT * FROM {table}""")
+            else:
+                self.cur.execute(f"""SELECT * FROM {table} LIMIT {limit}""")
+            records = self.cur.fetchall()
+            return records
+        except mysql.connector.errors.Error as err:
+            print(err)
+            return f"Error Occured while reading Database: {err}"
+
+    def close_connection(self):
+        self.cur.close()
+        self.conn.close()
+
 
 if __name__ == "__main__":
-    MyDatabase()
+    db = MyDatabase()
+    sample_table_name = "books"
+    column_names = db.get_column_names(sample_table_name)
+    all_tables = db.get_tables_name()
+    db.get_records(sample_table_name)
+    # print(f"Column names of the table '{all_tables}':")
+    db.close_connection()
